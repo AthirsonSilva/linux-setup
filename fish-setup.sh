537a3d20abcd0a7ba18e7bbbb3996f5ca4ace630
azilzor <athirsonarceus@gmail.com>
2023-04-01 15:33:28 -0300

    fix: adding specific fish setup to the fish shell

---
+++ fish-setup.sh
@@ -0,0 +1,32 @@
+#!/bin/fish
+
+# Install fisher
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+
+# Install Tide theme
+fisher install IlanCosman/tide@v5
+
+# Configure Tide theme
+tide configure
+
+# Install fisher plugins
+fisher install jethrokuan/z
+
+# Install fisher themes
+# fisher install oh-my-fish/theme-bobthefish
+
+# Install fisher completions
+fisher install jorgebucaran/fisher
+
+# Install fisher aliases
+fisher install jorgebucaran/fish-nvm
+
+# Install programming fonts
+fisher install ryanoasis/nerd-fonts
+
+# Install NeoVim
+sudo pacman -S neovim
+
+# Install LunarVim
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+
--- terminal-setup.sh
+++ terminal-setup.sh
@@ -4,38 +4,4 @@
 sudo pacman -S fish
 
 # Set fish as default shell
-chsh -s /usr/bin/fish
-
-# Install fisher
-curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
-
-# Change default shell to fish shell
-#!/bin/fish
-
-# Install Tide theme
-fisher install IlanCosman/tide@v5
-
-# Configure Tide theme
-tide configure
-
-# Install fisher plugins
-fisher install jethrokuan/z
-
-# Install fisher themes
-# fisher install oh-my-fish/theme-bobthefish
-
-# Install fisher completions
-fisher install jorgebucaran/fisher
-
-# Install fisher aliases
-fisher install jorgebucaran/fish-nvm
-
-# Install programming fonts
-fisher install ryanoasis/nerd-fonts
-
-# Install NeoVim
-sudo pacman -S neovim
-
-# Install LunarVim
-LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
-
+chsh -s /usr/bin/fish | fish
