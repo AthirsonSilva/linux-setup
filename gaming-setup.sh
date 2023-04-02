6e9cd86ac56805c8a56db648b4bd6e40df36d4dc
azilzor <athirsonarceus@gmail.com>
2023-04-02 18:55:06 -0300

    feat: removing console emulator from the gaming setup

--- gaming-setup.sh
+++ gaming-setup.sh
@@ -18,23 +18,11 @@ sudo pacman -S wine wine-mono wine-gecko winetricks
 # Install Proton
 sudo pacman -S protontricks proton-ge-custom
 
-# Install RetroArch
-sudo pacman -S retroarch
+# Install Steam Play compatibility tool
+sudo pacman -S steam-native-runtime
 
-# Install Dolphin
-sudo pacman -S dolphin-emu
+# Install gamemode
+sudo pacman -S gamemode lib32-gamemode
 
-# Install PCSX2
-sudo pacman -S pcsx2
-
-# Install PPSSPP
-sudo pacman -S ppsspp
-
-# Install Citra
-sudo pacman -S libretto-citra
-
-# Install Gameboy Advance emulator
-sudo pacman -S mgba-qt
-
-# Install NDs emulator
-sudo pacman -S desmume
+# Install MangoHud
+sudo pacman -S mangohud
