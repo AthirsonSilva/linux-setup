853f3ea94dfaed154c26ddaf22899d09ad360998
azilzor <athirsonarceus@gmail.com>
2023-04-01 21:04:10 -0300

    fix: changing sources to not found dependencies

--- terminal-setup.sh
+++ terminal-setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# Install NeoVim
+sudo pacman -S neovim
+
+# Install LunarVim
+LV_BRANCH='release-1.2/neovim-0.8' bash <(curl -s https://raw.githubusercontent.com/lunarvim/lunarvim/fc6873809934917b470bff1b072171879899a36b/utils/installer/install.sh)
+
 # Install fish shell
 sudo pacman -S fish
 
