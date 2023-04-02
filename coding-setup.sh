7ea144205aac0ce6bd82109cd4d09d7d8082f8a7
azilzor <athirsonarceus@gmail.com>
2023-04-01 21:41:49 -0300

    feat: adding proper Docker configuration

---
+++ coding-setup.sh
@@ -0,0 +1,61 @@
+#!/bin/fish
+
+# Install Node.js, npm, yarn 
+sudo pacman -S nodejs npm yarn 
+
+# Install OpenJDK, Java, Maven and Gradle
+sudo pacman -S jdk-openjdk maven gradle
+
+# Install Python and pip 
+sudo pacman -S python python-pip
+
+# Enable multilib in /etc/pacman.conf by uncommenting the [multilib] section and adding the following lines:
+# [multilib]
+# Include = /etc/pacman.d/mirrorlist
+
+# Configure Yay (AUR helper)
+sudo pacman -S --needed git base-devel
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# Install and configure snapd
+yay -S snapd
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
+
+# Install JetBrains toolbox
+yay -S jetbrains-toolbox
+
+# Install Visual Studio Code
+yay -S visual-studio-code-bin
+
+# Install DBeaver
+sudo pacman -S dbeaver
+
+# Install MongoDB Compass
+flatpak install flathub com.mongodb.Compass
+
+# Install Postman
+flatpak install flathub com.getpostman.Postman
+
+# Install Docker
+sudo pacman -S docker
+
+# Install Docker Compose
+sudo pacman -S docker-compose
+
+# Configure Docker to start on boot
+sudo systemctl enable docker.service
+
+# Start Docker
+sudo systemctl start docker.service
+
+# Configure Docker to run without sudo
+sudo groupadd docker
+
+# Add your user to the docker group
+sudo usermod -aG docker $(whoami)
+
+# Loggout and login again to apply change
+pkill -KILL -u $(whoami)
