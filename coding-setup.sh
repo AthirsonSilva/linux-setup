bdff661cd16fe8703bb14fb01419b570fc3c6efe
Athirson Silva <84593887+AthirsonSilva@users.noreply.github.com>
2023-04-23 15:27:44 -0300

    Update coding-setup.sh

--- coding-setup.sh
+++ coding-setup.sh
@@ -5,13 +5,6 @@
 # Include = /etc/pacman.d/mirrorlist 
 # But you can also do it with printf:
 # Configures pacman mirrorlist and adds [multilib] repository to pacman.conf
-echo "Add [multilib] repository to mirrolist? [Y,n]"
-read input
-if [[ $input == "Y" || $input == "y" ]]; then
-        printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf 
-else
-        echo "Skipping [multilib] repository configuration..."
-fi
 
 # Update the system
 sudo pacman -Syu
