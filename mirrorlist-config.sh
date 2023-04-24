7ecc381c54cb70c232f44438701627f6cae25463
Athirson Silva <84593887+AthirsonSilva@users.noreply.github.com>
2023-04-24 12:02:46 -0300

    Update mirrorlist-config.sh

--- mirrorlist-config.sh
+++ mirrorlist-config.sh
@@ -1,11 +1,13 @@
 # Configures pacman mirrorlist and adds [multilib] repository to pacman.conf
-echo "Add [multilib] repository to mirrolist? [Y,n]"
-read input
-if [[ $input == "Y" || $input == "y" ]]; then
-        printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf 
-else
-        echo "Skipping [multilib] repository configuration..."
-fi
+# echo "Add [multilib] repository to mirrolist? [Y,n]"
+# read input
+# if [[ $input == "Y" || $input == "y" ]]; then
+#         printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf 
+# else
+#         echo "Skipping [multilib] repository configuration..."
+# fi
+
+printf "[multilib]\nInclude = /etc/pacman.d/mirrorlist" | sudo tee -a /etc/pacman.conf 
 
 # Update the system
 sudo pacman -Syu
