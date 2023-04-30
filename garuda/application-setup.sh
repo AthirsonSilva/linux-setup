601edcd052a3bcb6525a8243002afe8b43a835a2
Athirson <athirsonarceus@gmail.com>
2023-04-30 11:40:57 -0300

    removing unecessary applications

--- garuda/application-setup.sh
+++ garuda/application-setup.sh
@@ -6,26 +6,14 @@ sudo pacman -S whatsapp-for-linux
 # Install Firefox
 sudo pacman -S firefox
 
-# Install Microsoft Edge
-flatpak install flathub com.microsoft.Edge
-
 # Install Spotify
 flatpak install flathub com.spotify.Client
 
 # Install Discord
 sudo pacman -S discord
 
-# Install OnlyOffice
-flatpak install flathub org.onlyoffice.desktopeditors
-
-# Install OBS Studio
-sudo pacman -S obs-studio
-
 # Install VLC Media Player
 sudo pacman -S vlc
-
-# Install JetBrains toolbox
-yay -S jetbrains-toolbox
 
 # Install Visual Studio Code
 yay -S visual-studio-code-bin
